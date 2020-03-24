b46cb9d5541befae5a9672af181a23ad07e485d2
programmer0152 <abou.w@hotmail.com>
2020-03-24 18:33:12 +0100

    Added some of my existing functions and cleaned them up a bit and added the make_tables file.

---
+++ MongoDB_to_MySQL.py
@@ -0,0 +1,71 @@
+import mysql.connector
+from pymongo import MongoClient
+from foreign_key_links import *
+
+cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', port='3307', database='huwebshop')
+cursor = cnx.cursor()
+
+limit = -1
+client = MongoClient('localhost', 27017)
+db = client["huwebshop"]
+
+
+def get_values(normalized, collection, values, get_fk):
+    counter = 0
+    normalized_list = []
+    upload_values = []
+    for entry in collection.find():
+        try:
+            upload = [counter]
+            for value in values:
+                if "-" in value:
+                    vl = value.split("-")
+                    upload.append(entry[vl[0]][vl[1]])
+                elif value == "?":
+                    upload.append(get_fk(entry))
+                else:
+                    upload.append(entry[value])
+
+            if not [i for i in upload if i in normalized_list] and normalized or not normalized:
+                upload_values.append(tuple(upload))
+                normalized_list.extend(upload)
+                counter += 1
+        except KeyError:
+            continue
+
+        if counter == limit and not normalized and limit != -1:
+            break
+
+    return upload_values
+
+
+def set_values(table_name, upload_values, headers):
+    parameters = ""
+    value_string = ""
+    for header in headers:
+        parameters += header + ","
+        value_string += "%s,"
+    parameters = parameters[:-1]
+    value_string = value_string[:-1]
+
+    cursor.executemany(f"INSERT INTO {table_name}({parameters}) VALUES ({value_string})", upload_values)
+    cnx.commit()
+
+
+def create_table(normalized, table_name, db_name, values, headers, get_fk=None):
+    collection = db[db_name]
+    cursor.execute(f"DELETE FROM {table_name};")
+
+    upload_values = get_values(normalized, collection, values, get_fk)
+    set_values(table_name, upload_values, headers)
+
+    print(f"{table_name} table uploaded using {db_name} database.")
+
+
+if __name__ == "__main__":
+    # init()
+    create_table(True, "gender", "products", ["gender"], ["idgender", "gendernaam"])
+    create_table(True, "brand", "products", ["brand"], ["idbrand", "brandnaam"])
+    create_table(False, "profile", "profiles", ["order-count", "recommendations-segment"], ["id", "`order.count`", "recmonendation_segment"])
+    create_table(False, "session", "sessions", ["has_sale", "segment", "?"], ["id", "has_sale", "preferences", "profile_id"], link_profile_session)
+    cnx.close()
---
+++ make_tables.py
@@ -0,0 +1,82 @@
+import psycopg2
+
+c = psycopg2.connect("dbname=postgres user=postgres password=postgres")  # TODO: edit this.
+cur = c.cursor()
+
+
+# Creating all the tables that have to be normalised.
+cur.execute("DROP TABLE IF EXISTS brand CASCADE")
+cur.execute("DROP TABLE IF EXISTS category CASCADE")
+cur.execute("DROP TABLE IF EXISTS sub_category CASCADE")
+cur.execute("DROP TABLE IF EXISTS sub_sub_category CASCADE")
+cur.execute("DROP TABLE IF EXISTS color CASCADE")
+cur.execute("DROP TABLE IF EXISTS gender CASCADE")
+
+cur.execute("""CREATE TABLE brand(_id INT PRIMARY KEY, brand VARCHAR);""")
+cur.execute("""CREATE TABLE category(_id INT PRIMARY KEY, category VARCHAR);""")
+cur.execute("""CREATE TABLE sub_category(_id INT PRIMARY KEY, sub_category VARCHAR);""")
+cur.execute("""CREATE TABLE sub_sub_category(_id INT PRIMARY KEY, sub_sub_category VARCHAR);""")
+cur.execute("""CREATE TABLE color(_id INT PRIMARY KEY, color VARCHAR);""")
+cur.execute("""CREATE TABLE gender(_id INT PRIMARY KEY, gender VARCHAR);""")
+
+# Creating the rest of the tables.
+cur.execute("DROP TABLE IF EXISTS profiles CASCADE")
+cur.execute("DROP TABLE IF EXISTS sessions CASCADE")
+cur.execute("DROP TABLE IF EXISTS products CASCADE")
+cur.execute("DROP TABLE IF EXISTS viewed_products CASCADE")
+cur.execute("DROP TABLE IF EXISTS products_bought CASCADE")
+cur.execute("DROP TABLE IF EXISTS previously_recommended CASCADE")
+
+cur.execute("""CREATE TABLE profiles
+                (_id VARCHAR PRIMARY KEY, 
+                recommendation_segment VARCHAR,
+                order_count INT);""")
+
+cur.execute("""CREATE TABLE sessions
+                (sessions_id VARCHAR PRIMARY KEY, 
+                has_sale BIT,
+                device_brand VARCHAR,
+                os VARCHAR,
+                profid VARCHAR,
+                FOREIGN KEY (profid) REFERENCES profiles (_id));""")
+
+cur.execute("""CREATE TABLE products
+                (_id VARCHAR PRIMARY KEY, 
+                brand_id INT,
+                category_id INT,
+                sub_category_id INT,
+                sub_sub_category_id INT,
+                color_id INT,
+                gender_id INT,
+                price DECIMAL,
+                FOREIGN KEY (brand_id) REFERENCES brand (_id),
+                FOREIGN KEY (category_id) REFERENCES category (_id),
+                FOREIGN KEY (sub_category_id) REFERENCES sub_category (_id),
+                FOREIGN KEY (sub_sub_category_id) REFERENCES sub_sub_category (_id),
+                FOREIGN KEY (color_id) REFERENCES color (_id),
+                FOREIGN KEY (gender_id) REFERENCES gender (_id));""")
+
+cur.execute("""CREATE TABLE viewed_products
+                (_id INT PRIMARY KEY, 
+                product_id VARCHAR,
+                profile_id VARCHAR,
+                FOREIGN KEY (product_id) REFERENCES products (_id),
+                FOREIGN KEY (profile_id) REFERENCES profiles (_id));""")
+
+cur.execute("""CREATE TABLE products_bought
+                (_id INT PRIMARY KEY, 
+                product_id VARCHAR,
+                profile_id VARCHAR,
+                FOREIGN KEY (product_id) REFERENCES products (_id),
+                FOREIGN KEY (profile_id) REFERENCES profiles (_id));""")
+
+cur.execute("""CREATE TABLE previously_recommended
+                (_id INT PRIMARY KEY, 
+                product_id VARCHAR,
+                profile_id VARCHAR,
+                FOREIGN KEY (product_id) REFERENCES products (_id),
+                FOREIGN KEY (profile_id) REFERENCES profiles (_id));""")
+
+c.commit()
+cur.close()
+c.close()
